93a9a3448ee2429e2c20935038570d1961203b71
saisrujana <saisrujana1704@gmail.com>
2023-07-18 16:36:14 +0530

    change

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,6 +1,23 @@
-def fun(n):
-    if (n > 100):
-        return n - 5
-    return fun(fun(n+11))
+#s = "A man, a plan, a canal: Panama" # output : True
+#s = " "  # output : True
+s = " race a car"  # output : False
+s_list = [ i.lower() for i in s]
+s_list1 = []
+for i in s_list:
+    if i.isalnum() == True:
+        s_list1.append(i)
+s1 = "".join(s_list1)
 
-print(fun(45))
+print(s1 == s1[::-1])
+
+###
+s1 = ""
+for i in s:
+    if i.isalnum() == True:
+        i = i.lower()
+        s1 += i
+print(s1 == s1[::-1])
+
+###
+s1 = [i.lower() for i in s if i.isalnum() == True]
+print(s1 == s1[::-1])
